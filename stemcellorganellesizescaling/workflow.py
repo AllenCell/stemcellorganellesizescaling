499db25bf0b6aaded9b63c4691136f1dadb9b777
tknijnen <theo.knijnenburg@alleninstitute.org>
2020-10-06 20:20:17 -0700

    Adding Unix vs Windows flag

--- stemcellorganellesizescaling/workflow.py
+++ stemcellorganellesizescaling/workflow.py
@@ -5,9 +5,10 @@
 import logging
 from pathlib import Path
 import sys, importlib
+import os, platform
 
 # Third party
-importlib.reload(sys.modules['stemcellorganellesizescaling.analyses.data_prep'])
+importlib.reload(sys.modules["stemcellorganellesizescaling.analyses.data_prep"])
 from stemcellorganellesizescaling.analyses.data_prep import outlier_removal
 
 # Relative
@@ -20,14 +21,17 @@ log = logging.getLogger(__name__)
 ###############################################################################
 
 #%% Directories
-data_root = Path("E:/DA/Data/scoss/Data/")
-pic_root = Path("E:/DA/Data/scoss/Pics/")
+if platform.system() == "Windows":
+    data_root = Path("E:/DA/Data/scoss/Data/")
+    pic_root = Path("E:/DA/Data/scoss/Pics/")
+elif platform.system() == "Linux":
+    data_root = Path("/allen/aics/modeling/theok/Projects/Data/scoss/Data/")
+    pic_root = Path("/allen/aics/modeling/theok/Projects/Data/scoss/Pics/")
 dirs = []
 dirs.append(data_root)
 dirs.append(pic_root)
 
 #%% Data preparation
-tableIN = 'SizeScaling_20200828.csv'
-tableOUT = 'SizeScaling_20200828_clean.csv'
+tableIN = "SizeScaling_20200828.csv"
+tableOUT = "SizeScaling_20200828_clean.csv"
 outlier_removal(dirs, tableIN, tableOUT)
-
