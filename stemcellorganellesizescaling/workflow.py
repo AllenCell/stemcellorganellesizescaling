cc980254f0d38630864bdbc7df55a5b2735d9c49
tknijnen <theo.knijnenburg@alleninstitute.org>
2020-12-15 20:13:14 -0800

    Prepping for run with new metrics

--- stemcellorganellesizescaling/workflow.py
+++ stemcellorganellesizescaling/workflow.py
@@ -75,11 +75,11 @@ dirs.append(pic_root)
 
 #%% Data preparation - Initial Parsing
 # print('##################### Data preparation - Initial Parsing #####################')
-tableIN = "/allen/aics/assay-dev/MicroscopyOtherData/Viana/projects/cell_shape_variation/local_staging_beta/shapemode/manifest.csv"
-featIN = "/allen/aics/assay-dev/MicroscopyOtherData/Viana/resources/qcb/data-raw/production/feature"
-tableSNIP = "Manifest_snippet_20201215.csv"
-tableOUT = "SizeScaling_20201215.csv"
-initial_parsing(dirs, tableIN, featIN, tableSNIP, tableOUT)
+# tableIN = "/allen/aics/assay-dev/MicroscopyOtherData/Viana/projects/cell_shape_variation/local_staging_beta/shapemode/manifest.csv"
+# featIN = "/allen/aics/assay-dev/MicroscopyOtherData/Viana/resources/qcb/data-raw/production/feature"
+# tableSNIP = "Manifest_snippet_20201215.csv"
+# tableOUT = "SizeScaling_20201215.csv"
+# initial_parsing(dirs, tableIN, featIN, tableSNIP, tableOUT)
 
 #%% Data preparation - Outlier Removal
 # print('##################### Outlier removal is done more upstream and typically not run anymore as part of the size scaling workflow #####################'
@@ -90,9 +90,9 @@ initial_parsing(dirs, tableIN, featIN, tableSNIP, tableOUT)
 # outlier_removal(dirs, tableIN, tableOUT, tableOUTL)
 
 #%% Data preparation - Diagnostic violins
-print('##################### Data preparation - Diagnostic violins #####################')
-tableIN = "SizeScaling_20201215.csv"
-diagnostic_violins(dirs, tableIN)
+# print('##################### Data preparation - Diagnostic violins #####################')
+# tableIN = "SizeScaling_20201215.csv"
+# diagnostic_violins(dirs, tableIN)
 
 #%% Computing statistics - Compensation analysis
 # print('##################### Computing statistics - Compensation analysis #####################')
@@ -101,25 +101,25 @@ diagnostic_violins(dirs, tableIN)
 # compensate(dirs, tableIN, tableOUT)
 
 #%% Computing statistics - Pairwise statistics
-# print('##################### Computing statistics - Pairwise statistics #####################')
-# tableIN = "SizeScaling_20201102.csv"
-# table_compIN = "SizeScaling_20201102_comp.csv"
-# statsOUTdir = "Stats_20201102"
-# pairwisestats(dirs, tableIN, table_compIN, statsOUTdir)
+print('##################### Computing statistics - Pairwise statistics #####################')
+tableIN = "SizeScaling_20201215.csv"
+table_compIN = "SizeScaling_20201215_comp.csv"
+statsOUTdir = "Stats_20201215"
+pairwisestats(dirs, tableIN, table_compIN, statsOUTdir, COMP_flag=False)
 
 #%% Computing statistics - Explained variance of composite models
-# print('##################### Computing statistics - Composite models #####################')
-# tableIN = "SizeScaling_20201102.csv"
-# statsOUTdir = "Stats_20201102"
-# compositemodels_explainedvariance(dirs, tableIN, statsOUTdir)
+print('##################### Computing statistics - Composite models #####################')
+tableIN = "SizeScaling_20201215.csv"
+statsOUTdir = "Stats_20201215"
+compositemodels_explainedvariance(dirs, tableIN, statsOUTdir)
 
 #%% Computing statistics - Scaling statistics
-# print(
-#     "##################### Computing statistics - Scaling statistics #####################"
-# )
-# tableIN = "SizeScaling_20201102.csv"
-# scaleOUTdir = "Scale_20201102"
-# scaling_stats(dirs, tableIN, scaleOUTdir)
+print(
+    "##################### Computing statistics - Scaling statistics #####################"
+)
+tableIN = "SizeScaling_20201215.csv"
+scaleOUTdir = "Scale_20201215"
+scaling_stats(dirs, tableIN, scaleOUTdir)
 
 #%% Plotting scatterplots - Cell and nuclear metrics
 # print('##################### Plotting scatterplots - Cell and nuclear metrics #####################')
